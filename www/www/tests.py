4481e0ae0e25cf51b65783d2d8bff4a908e55476
Bernhard Essl <bernhardessl@gmail.com>
2015-10-25 10:46:50 +0100

    clean up

--- www/www/tests.py
+++ www/www/tests.py
@@ -3,80 +3,80 @@ import transaction
 import datetime
 
 from pyramid import testing
-from .models import DBSession, Measurement
-from www.views import add, current
+from .models import DBSession, Log
+from www.views import *
 
 def _initTestingDB():
     from sqlalchemy import create_engine
     from www.models import (
         DBSession,
-        Measurement,
+        Log,
         Base
         )
     engine = create_engine('sqlite://')
     Base.metadata.create_all(engine)
     DBSession.configure(bind=engine)
     with transaction.manager:
-        model = Measurement(id=1, at=datetime.datetime.now(), temperature=33, humidity=41)
+        model = Log(id=1, at=datetime.datetime.now(), temperature=33, humidity=41)
         DBSession.add(model)
-        model = Measurement(id=2, at=datetime.datetime.now(), temperature=13, humidity=40)
+        model = Log(id=2, at=datetime.datetime.now(), temperature=13, humidity=40)
         DBSession.add(model)
     return DBSession
 
 
-class CurrentDataTests(unittest.TestCase):
-    def setUp(self):
-        self.session = _initTestingDB()
-        self.config = testing.setUp()
-        self.config.add_route('current', 'current.json')
-
-    def tearDown(self):
-        self.session.remove()
-        testing.tearDown()
-
-    def test_current_data(self):
-        request = testing.DummyRequest()
-        response = current(request)
-        self.assertEqual(response["temperature"], 13)
-        self.assertEqual(response["humidity"], 40)
-
-
-class AddMeasurementDataTests(unittest.TestCase):
-    def setUp(self):
-        self.session = _initTestingDB()
-        self.config = testing.setUp()
-        self.config.add_route('add', 'add/')
-
-    def tearDown(self):
-        self.session.remove()
-        testing.tearDown()
-
-    def test_only_post_request_allowed(self):
-        request = testing.DummyRequest(params={'t': 12, 'h': 34}, post=None)
-        response = add(request)
-        self.assertEqual(response.status_code, 500)
-
-    def test_only_full_data_request_allowed(self):
-        request = testing.DummyRequest(params={'t': 12}, post=True)
-        response = add(request)
-        self.assertEqual(response.status_code, 500)
-
-        request2 = testing.DummyRequest(params={'h': 99}, post=True)
-        response2 = add(request2)
-        self.assertEqual(response2.status_code, 500)
-
-    def test_save_request(self):
-        request = testing.DummyRequest(params={'t': 23, 'h': 24}, post=True)
-        response = add(request)
-        self.assertEqual(response.status_code, 200)
-
-        m = self.session.query(Measurement).filter_by(temperature=23).one()
-        self.assertEqual(m.humidity, 24)
-
-        count_current = len(self.session.query(Measurement).all())
-        self.assertEqual(count_current, 3)
-
-        self.assertEqual(m.id, 3)
-        self.assertEqual(m.humidity, 24)
-        self.assertEqual(m.temperature, 23)
-        self.assertEqual(m.at.strftime("%d%m%Y%H"), datetime.datetime.now().strftime("%d%m%Y%H"))
+# class CurrentDataTests(unittest.TestCase):
+#     def setUp(self):
+#         self.session = _initTestingDB()
+#         self.config = testing.setUp()
+#         self.config.add_route('current', 'current.json')
+#
+#     def tearDown(self):
+#         self.session.remove()
+#         testing.tearDown()
+#
+#     def test_current_data(self):
+#         request = testing.DummyRequest()
+#         response = current(request)
+#         self.assertEqual(response["temperature"], 13)
+#         self.assertEqual(response["humidity"], 40)
+#
+#
+# class AddMeasurementDataTests(unittest.TestCase):
+#     def setUp(self):
+#         self.session = _initTestingDB()
+#         self.config = testing.setUp()
+#         self.config.add_route('add', 'add/')
+#
+#     def tearDown(self):
+#         self.session.remove()
+#         testing.tearDown()
+#
+#     def test_only_post_request_allowed(self):
+#         request = testing.DummyRequest(params={'t': 12, 'h': 34}, post=None)
+#         response = add(request)
+#         self.assertEqual(response.status_code, 500)
+#
+#     def test_only_full_data_request_allowed(self):
+#         request = testing.DummyRequest(params={'t': 12}, post=True)
+#         response = add(request)
+#         self.assertEqual(response.status_code, 500)
+#
+#         request2 = testing.DummyRequest(params={'h': 99}, post=True)
+#         response2 = add(request2)
+#         self.assertEqual(response2.status_code, 500)
+#
+#     def test_save_request(self):
+#         request = testing.DummyRequest(params={'t': 23, 'h': 24}, post=True)
+#         response = add(request)
+#         self.assertEqual(response.status_code, 200)
+#
+#         m = self.session.query(Log).filter_by(temperature=23).one()
+#         self.assertEqual(m.humidity, 24)
+#
+#         count_current = len(self.session.query(Log).all())
+#         self.assertEqual(count_current, 3)
+#
+#         self.assertEqual(m.id, 3)
+#         self.assertEqual(m.humidity, 24)
+#         self.assertEqual(m.temperature, 23)
+#         self.assertEqual(m.at.strftime("%d%m%Y%H"), datetime.datetime.now().strftime("%d%m%Y%H"))
--- www/www/views.py
+++ www/www/views.py
@@ -5,46 +5,46 @@ from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
-    Measurement,
+    Log,
     )
 
 
 @view_config(route_name='home', renderer='templates/start.jinja2')
 def start(request):
     try:
-        one = DBSession.query(Measurement).first()
+        one = DBSession.query(Log).first()
     except DBAPIError:
         return Response("Database Error", content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'www'}
+    return {'one': one}
 
-
-@view_config(route_name='add')
-def add(request):
-    # only post requests are allowed
-    if request.method is not "POST":
-        return Response("Request Error!", content_type='text/plain', status_int=500)
-
-    # check for incoming data
-    try:
-        humidity = request.params['h']
-        temperature = request.params['t']
-    except(KeyError):
-        return Response("Request Error 2!", content_type='text/plain', status_int=500)
-    else:
-        humidity = int(humidity)
-        temperature = int(temperature)
-
-    m = Measurement(temperature=temperature, humidity=humidity)
-    DBSession.add(m)
-
-    return Response('add done', status_int=200)
-
-
-@view_config(route_name='current', renderer='json')
-def current(request):
-    m = DBSession.query(Measurement).order_by(Measurement.at.desc()).first()
-    return {
-        'humidity': m.humidity,
-        'temperature': m.temperature,
-        'at': str(m.at)
-    }
+#
+# @view_config(route_name='add')
+# def add(request):
+#     # only post requests are allowed
+#     if request.method is not "POST":
+#         return Response("Request Error!", content_type='text/plain', status_int=500)
+#
+#     # check for incoming data
+#     try:
+#         humidity = request.params['h']
+#         temperature = request.params['t']
+#     except(KeyError):
+#         return Response("Request Error 2!", content_type='text/plain', status_int=500)
+#     else:
+#         humidity = int(humidity)
+#         temperature = int(temperature)
+#
+#     m = Measurement(temperature=temperature, humidity=humidity)
+#     DBSession.add(m)
+#
+#     return Response('add done', status_int=200)
+#
+#
+# @view_config(route_name='current', renderer='json')
+# def current(request):
+#     m = DBSession.query(Measurement).order_by(Measurement.at.desc()).first()
+#     return {
+#         'humidity': m.humidity,
+#         'temperature': m.temperature,
+#         'at': str(m.at)
+#     }
